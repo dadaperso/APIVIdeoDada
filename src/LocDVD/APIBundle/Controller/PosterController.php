0b9804fb3591a411fe36711d1dc5f1096f9bcdbd
dada <damien.lejart@orange.fr>
2016-01-25 19:22:37 +0100

    fixing image rendering issue

--- src/LocDVD/APIBundle/Controller/PosterController.php
+++ src/LocDVD/APIBundle/Controller/PosterController.php
@@ -12,14 +12,14 @@ namespace LocDVD\APIBundle\Controller;
 use FOS\RestBundle\Controller\Annotations;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\View;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 
-class PosterController extends Controller
+class PosterController extends FOSRestController
 {
     /**
     *
-    * @Get("/poster/{oid}")
+    * @Get("/poster/{oid}.{_format}")
     * @param Request $request
     * @return array
     * @View()
@@ -27,13 +27,28 @@ class PosterController extends Controller
 
     public function getImgPosterAction(Request $request, $oid)
     {
-        $posterRepo = $this->getDoctrine()->getRepository('LocDVDAPIBundle:Poster');
+        $dirPosterPath = $this->get('kernel')->getRootDir().'/../web/bundles/locdvdapi/images/poster/';
+        $fileName = $dirPosterPath.$oid.'.jpg';
+//        $fileName = $this->get('templating.helper.assets')->getUrl($dirPosterPath.'poster'.$oid.'.jpg');
 
-        $imgPoster = $posterRepo->getPoster($oid);
+        $this->get('logger')->addDebug('path to poster: '.$fileName);
 
-        return array(
-            'oid' => $oid,
-            'imgPoster' => base64_encode($imgPoster),
-        );
+        if(!file_exists($fileName)){
+            $posterRepo = $this->getDoctrine()->getRepository('LocDVDAPIBundle:Poster');
+
+            $imgPoster = $posterRepo->getPoster($oid);
+
+            $fp = fopen($fileName, 'w');
+            fwrite($fp, $imgPoster);
+
+            fclose($fp);
+        }
+
+        header('Content-Type: image/jpeg');
+
+       imagejpeg(imagecreatefromjpeg($fileName));
+        die();
+
+      // return $this->redirect($fileName);
     }
 }
